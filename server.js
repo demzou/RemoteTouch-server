0a84fe044029b95dacf1e24678d45f1dcf4e422e
demzou <clemence.debaig@gmail.com>
2021-01-06 12:11:10 +0000

    Clean up code + comments

--- server.js
+++ server.js
@@ -1,29 +1,12 @@
 
-/*
- * Dependencies
- */
-const express = require('express');
-
 
 /*
  * Config
  */
+const express = require('express');   //Dependencies
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(`${__dirname}/public`));
-
-
-/*
- * User Routes
- */
-
-// app.get('/', (req, res) => {    // Direct traffic to splash screen first
-//   res.redirect('/welcome');
-// });
-
-// app.get('*', (req, res) => {
-//   res.redirect('/welcome');
-// });
 
 
 
@@ -31,8 +14,7 @@ app.use(express.static(`${__dirname}/public`));
  * socket.io
  */
 
-
-const http = require("http");
+const http = require("http");         //Dependencies
 const server = http.createServer(app);
 server.listen(port);
 
@@ -43,103 +25,49 @@ const { match } = require('assert');
 let io = socketio.listen(server);
 console.log(`Listening for socket connections on port ${port}`);
 
+
+//-------------------------------------
+// Manage behaviour of all the devices in parallel, allowing for multi-clicks
+// Check if someone is clicking on a button and send buzz instructions to everyone
 let buttonCount = 0;
-
-// const sockets = []
-
-// function sendData() {
-  
-//   if (sockets.length > 0) {
-//     if (buttonCount > 0) {
-//       sockets[0].broadcast.emit(1)
-//     }
-//     else {
-//       sockets[0].broadcast.emit(0)
-//     }  
-//   }
-
-//   buttonCount = 0
-  
-// }
 
 setInterval( function() {
   sendBuzz();
-}, 300);
+}, 300);                            // sampling every 300 ms
 
 function sendBuzz() {
-if(buttonCount > 0) {
-  io.sockets.emit('buzz', '1');
-  console.log('sent buzz: 1');
-} else { 
-  io.sockets.emit('buzz', '0');
-  console.log('sent Buzz: 0');
-}
-buttonCount = 0;
+  if(buttonCount > 0) {
+    io.sockets.emit('buzz', '1');   //if at least one person clicked, everyone is buzzing
+    //console.log('sent buzz: 1');
+  } else { 
+    io.sockets.emit('buzz', '0');
+    //console.log('sent Buzz: 0');
+  }
+buttonCount = 0;                    // reset count at the end of the interval
 }
 
-
-//let mySocket = null
+//-------------------------------------
 // Register a callback function to run when we have an individual connection
 // This is run for each individual client that connects
 io.sockets.on('connection',
   // Callback function to call whenever a socket connection is made
   function (socket) {
 
-    //sockets.push(socket)
-
     // Print message to the console indicating that a new client has connected
     console.log("New client: " + socket.id);
-    socket.broadcast.emit('event', "hello");     // Send new lenth to everyone
-    // io.to(socket.id).emit('numClients', clientList.length); 
-    // io.to(socket.id).emit('mode', mode);                        // Send current mode to new client
 
 
-    socket.on('event',
-    function(data) {
-       console.log(data);
-      }
-    );
-
-    socket.on('test',
-    function(data) {
-       console.log(data);
-      }
-    );
-
+    //----------> Manage button clicks
     socket.on('button',
     function(data) {
         if(data == 1) {
-          buttonCount++;
+          buttonCount++;           // if someone clicks, increase buttonCount
         }
-       console.log('button: ' + data);
-       //socket.broadcast.emit('button', data);
+       //console.log('button: ' + data);
       }
     );
-
-    // setInterval( function() {
-    //     sendBuzz(socket);
-    // }, 1000);
-
-    // function sendBuzz(socket) {
-    //   if(buttonCount > 0) {
-    //     socket.broadcast.emit('buzz', '1');
-    //     console.log('sent buzz: 1');
-    //   } else { 
-    //     socket.broadcast.emit('buzz', '0');
-    //     console.log('sent Buzz: 0');
-    //   }
-    //   buttonCount = 0;
-    // }
-
-    // setInterval( function() {
-    //     sendHello(socket);
-    //     console.log('sent Hello');
-    // }, 10000);
-
-    // function sendHello(socket) {
-    //     socket.broadcast.emit('event', "hello");
-    // }
-        
+    //----------
+      
     
     // Specify a callback function to run when the client disconnects
     socket.on('disconnect',
